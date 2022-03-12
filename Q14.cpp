ff92d6118dcfc6ee7a7fc4bad2092d8d6ebf57c1
TheSuperKDK <sreenikalarikkal@gmail.com>
2022-03-12 14:58:45 +0530

    Q14

---
+++ Q14.cpp
@@ -0,0 +1,38 @@
+// Program to read admission number of N students in a class and search for a given admission number in the list. Use linear search of searching.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int array[100], n, check, present = 0;
+
+	cout << "Enter Number of Students : ";
+	cin >> n;
+
+	cout << "Enter Admission Numbers for Array\n";
+	for (int i = 0; i < n; i++) {
+		cin >> array[i];
+	}
+
+	// read Admission numbers
+	cout << "Addmission Numbers of Students are\n";
+	for (int i = 0; i < n; i++) {
+		cout << array[i] << '\n';
+	}
+
+	cout << "Enter Addmission Number to Check\n";
+	cin >> check;
+
+	// search Admission numbers
+	for (int i = 0; i < n; i++) {
+		if (array[i] == check) {
+			present = 1;
+			break;
+		}
+	}
+
+	if (check) cout << "Addmision Number is Present";
+	else cout << "Addmision Number is Not Present";
+}
+
