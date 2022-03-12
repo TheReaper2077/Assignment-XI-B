c943639ab3085f2a3e032a19abb851b6c25c51b0
TheSuperKDK <sreenikalarikkal@gmail.com>
2022-03-12 13:05:52 +0530

    upto Q6

--- Q6.cpp
+++ Q6.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int main(int, char**) {
-	double a, b, c, res1, res2, real, imaginary;
+	double a, b, c, res1 = 0, res2 = 0, real = 0, imaginary = 0;
 	cin >> a >> b >> c;
 
 	double discriminant = b*b - 4*a*c;
@@ -16,8 +16,8 @@ int main(int, char**) {
 
 		cout << res1;
 	} else if (discriminant > 0) {
-		res1 = (-b + (double)sqrt(discriminant)) / (double)2*a;
-		res2 = (-b - (double)sqrt(discriminant)) / (double)2*a;
+		res1 = (-b + sqrt(discriminant)) / 2*a;
+		res2 = (-b - sqrt(discriminant)) / 2*a;
 
 		cout << res1 << ", " << res2;
 	} else {
