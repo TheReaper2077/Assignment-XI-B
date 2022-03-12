d3d3c4797847070abee77cf0370b95c7fbb5e4f6
TheSuperKDK <sreenikalarikkal@gmail.com>
2022-03-12 15:27:36 +0530

    Q16

---
+++ Q16.cpp
@@ -0,0 +1,36 @@
+// Create a square matrix to display the same in matrix form. Find the sum of
+// leading diagonal elements(from top left to bottom right) and off diagonal
+// elements(top right to bottom left) separately.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int matrix[100][100];
+	int n = 5;
+
+	cout << "Enter The numbers\n";
+	for (int y = 0; y < n; y++) {
+		for (int x = 0; x < n; x++) {
+			cin >> matrix[y][x];
+		}
+	}
+
+	cout << "Numbers in Matrix are\n";
+	for (int y = 0; y < n; y++) {
+		for (int x = 0; x < n; x++) {
+			cout << matrix[y][x] << '\t';
+		}
+
+		cout << '\n';
+	}
+
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += matrix[i][i];
+	}
+
+	cout << "Sum of Diagonal Elements = " << sum;
+}
+
